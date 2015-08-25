6971f60152526a7d6e2af13e00165f586f082a7d
paulsflanagan <paulsflanagan@gmail.com>
2015-08-25 21:03:19 +0100

    Update 17-SELECT-GROUP-BY-HAVING.sql
    Best way I have found of doing this is by creating a view and querying that view.

--- 17-SELECT-GROUP-BY-HAVING.sql
+++ 17-SELECT-GROUP-BY-HAVING.sql
@@ -1,6 +1,15 @@
-SELECT stars.starid AS starid, 
-COUNT(planets.planetid) + COUNT(moons.planetid) AS orbitals 
-FROM planets, stars, moons 
-WHERE stars.starid=planets.starid 
-AND planets.planetid=moons.planetid 
+BEGIN;
+
+CREATE VIEW v_orbital AS
+SELECT stars.starid, COUNT(planets.planetid)+COUNT(moons.planetid) AS orbitals
+FROM stars
+LEFT OUTER JOIN planets ON stars.starid=planets.starid
+LEFT OUTER JOIN moons ON planets.planetid=moons.planetid
 GROUP BY stars.starid;
+
+INSERT INTO hilight
+SELECT starid
+FROM v_orbital
+WHERE orbitals=(SELECT MAX(orbitals) FROM v_orbital);
+
+DROP VIEW v_orbital;
