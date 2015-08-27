da105e29a1b077d7860854eab1904db407a2e4e2
JCharante <ralphcharante@gmail.com>
2015-08-26 20:53:55 -0400

    14-clarified
    I hope I have the right idea, so these are supposed to be solutions so you can just copy+paste them into the tutorial instead of a guide that gives you an idea of what the solution is. If so then this is a solution that you can just copy+paste and run the query to solve the chapter.

--- 14-CREATE-TABLE-DROP-TABLE.sql
+++ 14-CREATE-TABLE-DROP-TABLE.sql
@@ -1,2 +1,15 @@
 CREATE TABLE colors (color INTEGER PRIMARY KEY, description TEXT);
-INSERT INTO colors (color, description) VALUES (1, "blue");
+INSERT INTO colors (color, description) VALUES (-3,'a');
+INSERT INTO colors (color, description) VALUES (-2,'b');
+INSERT INTO colors (color, description) VALUES (-1,'a');
+INSERT INTO colors (color, description) VALUES (0,'c');
+INSERT INTO colors (color, description) VALUES (1,'d');
+INSERT INTO colors (color, description) VALUES (2,'e');
+INSERT INTO colors (color, description) VALUES (3,'f');
+INSERT INTO colors (color, description) VALUES (4,'g');
+INSERT INTO colors (color, description) VALUES (5,'h');
+INSERT INTO colors (color, description) VALUES (6,'i');
+INSERT INTO colors (color, description) VALUES (7,'j');
+INSERT INTO colors (color, description) VALUES (8,'k');
+INSERT INTO colors (color, description) VALUES (9,'l');
+INSERT INTO colors (color, description) VALUES (10,'m');
